8920b07a15f5cb872bb4a9e686f693b7598316a5
Luiz G R Freitas <luizfreitasaca@gmail.com>
2024-09-25 22:07:24 -0300

    Restarting project after initial tests;
    Signed-off-by: Luiz G R Freitas <luizfreitasaca@gmail.com>

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
-use App\Http\Controllers\WalletTrackerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::controller(WalletTrackerController::class)->group(function () {
-    Route::get('/dashboard', 'index')->name('wallet-tracker.index');
-    Route::get('/add_expense', 'addExpense')->name('wallet-tracker.add');
 });
