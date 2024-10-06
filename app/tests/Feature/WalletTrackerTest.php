91d834900ae7368b9d74e08f411eb84d521c1037
Luiz G R Freitas <luizfreitasaca@gmail.com>
2024-10-06 17:32:06 -0300

    Changing status code on tests to start views development.
    Signed-off-by: Luiz G R Freitas <luizfreitasaca@gmail.com>

--- app/tests/Feature/WalletTrackerTest.php
+++ app/tests/Feature/WalletTrackerTest.php
@@ -16,28 +16,28 @@ class WalletTrackerTest extends TestCase
     {
         $response = $this->get('/home');
 
-        $response->assertStatus(204);
+        $response->assertStatus(200);
     }
 
     public function testTagsPageRoute(): void
     {
         $response = $this->get('/tags');
 
-        $response->assertStatus(204);
+        $response->assertStatus(200);
     }
 
     public function testExpensesPageRoute(): void
     {
         $response = $this->get('/expenses');
 
-        $response->assertStatus(204);
+        $response->assertStatus(200);
     }
 
     public function testStatisticsPageRoute(): void
     {
         $response = $this->get('/statistics');
 
-        $response->assertStatus(204);
+        $response->assertStatus(200);
     }
 
     public function testCreatingCorrectExpenses(): void
